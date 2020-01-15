50b3efa6d91a60040fd7d0bd029927abede3f1bd
Greg198616 <55443929+Greg198616@users.noreply.github.com>
2020-01-15 21:44:04 +0200

    + 2 блока
    добавлен блол surf & travel

--- app/js/main.js
+++ app/js/main.js
@@ -3,15 +3,36 @@ $(function(){
 $('.header__slider').slick({
 	infinite: true,
 	fade: true,
-	prevArrow:'<img class="slider-arrows slider-arrows_left"} src="images/arrows-left.svg" alt="">',
-	nextArrow:'<img class="slider-arrows slider-arrows_right"} src="images/arrows-right.svg" alt="">',
-	asNavFor: '.slider-dots',
+	prevArrow:'<img class="slider-arrows slider-arrows_left" src="images/arrows-left.svg" alt="">',
+	nextArrow:'<img class="slider-arrows slider-arrows_right" src="images/arrows-right.svg" alt="">',
+	asNavFor: '.slider-dotshead',
 });
 
-$('.slider-dots').slick({
+$('.slider-dotshead').slick({
 	slidesToShow: 4,
   	slidesToScroll: 4,
   	asNavFor: '.header__slider',
 });
-  
+
+ $('.surf-slider').slick({
+	slidesToShow: 4,
+  	slidesToScroll: 1,
+  	prevArrow:'<img class="slider-arrows slider-arrows_left" src="images/arrows-left.svg" alt="">',
+	nextArrow:'<img class="slider-arrows slider-arrows_right" src="images/arrows-right.svg" alt="">',
+	asNavFor: '.slider-map',
+}); 
+ $('.slider-map').slick({
+ 	slidesToShow: 8,
+  	slidesToScroll: 1,
+  	arrows: false,
+  	asNavFor: '.surf-slider',
+  	focusOnSelect: true,
+ });
+ $('.travel__slider').slick({
+ 	infinite: true,
+	fade: true,
+	prevArrow:'<img class="slider-arrows slider-arrows_left" src="images/arrows-left.svg" alt="">',
+	nextArrow:'<img class="slider-arrows slider-arrows_right" src="images/arrows-right.svg" alt="">',
+ });
+
 });
